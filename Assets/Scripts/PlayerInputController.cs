2d2ddcf508fe55daa1064ff7255e0154a332d273
leemoojin <hzm_deli@naver.com>
2024-05-14 03:04:48 +0900

    Init SpartaTown

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownController
+{
+    //lookMouse
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    //Input
+    public void OnMove(InputValue value)
+    {
+        //Debug.Log($"PlayerInputController, OnMove value: {value}");
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        //Debug.Log($"PlayerInputController, OnMove moveInput: {moveInput}");
+
+        CallMoveEvent(moveInput);
+    }
+    public void OnLook(InputValue value) 
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = _camera.ScreenToWorldPoint(newAim);
+        newAim = worldPos - ((Vector2)transform.position).normalized;
+
+        CallLookEvent(newAim);
+    }
+
+}
---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    private TopDownController _controller;
+    private Rigidbody2D _movementRb;
+
+    private Vector2 _movementDirection = Vector2.zero;
+
+    private void Awake()
+    {        
+        _controller = GetComponent<TopDownController>();
+        _movementRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _controller.OnMoveEvent += Move;
+    }
+
+    //방향성
+    private void Move(Vector2 vector)
+    {
+        _movementDirection = vector;
+    }
+
+    //물리 업데이트
+    private void FixedUpdate()
+    {
+        ApplyMovement(_movementDirection);
+    }
+   
+    private void ApplyMovement(Vector2 direction)
+    {   
+        //이동거리 증가
+        direction = direction * 5;
+        _movementRb.velocity = direction;
+        //Debug.Log($"TopDownMovement, _movementRb.velocity {_movementRb.velocity}");
+    }
+}
